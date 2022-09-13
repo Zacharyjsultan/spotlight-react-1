a85016a25188bbb0f6ec3c3f39d81586962b0a9a
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-09-13 13:00:26 -0700

    5/9

--- index.js
+++ index.js
@@ -71,7 +71,10 @@ export const getFirstTwoArgs = (a, b, ...rest) => {
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {}
+export const addSneakerCount = ({ shoes }) => {
+
+  return { sneakerCount: shoes.length}
+}
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
